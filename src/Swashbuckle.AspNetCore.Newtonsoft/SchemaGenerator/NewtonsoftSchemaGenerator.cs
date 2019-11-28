e874ed2200b51f65f438d1c2acdb52b3a05f9b81
domaindrivendev <domaindrivendev@gmail.com>
2019-11-28 12:27:09 +0000

    Remove IApiModelResolver in favor of per-serializer ISchemaGenerator + improve Nullable assignment

---
+++ src/Swashbuckle.AspNetCore.Newtonsoft/SchemaGenerator/NewtonsoftSchemaGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Swashbuckle.AspNetCore.Newtonsoft
+{
+    public class NewtonsoftSchemaGenerator : SchemaGeneratorBase
+    {
+        public NewtonsoftSchemaGenerator(SchemaGeneratorOptions generatorOptions, JsonSerializerSettings serializerSettings)
+            : base(generatorOptions)
+        {
+            var contractResolver = serializerSettings.ContractResolver ?? new DefaultContractResolver();
+
+            AddHandler(new FileTypeHandler());
+            AddHandler(new PolymorphicTypeHandler(generatorOptions, this));
+            AddHandler(new NewtonsoftPrimitiveTypeHandler(contractResolver, serializerSettings));
+            AddHandler(new NewtonsoftEnumHandler(contractResolver, serializerSettings));
+            AddHandler(new NewtonsoftDictionaryHandler(contractResolver, serializerSettings, this));
+            AddHandler(new NewtonsoftArrayHandler(contractResolver, serializerSettings, this));
+            AddHandler(new NewtonsoftObjectHandler(generatorOptions, contractResolver, serializerSettings, this));
+        }
+    }
+}
